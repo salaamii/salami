972e6b1e0dfdb66760d131d1cf2508f92ce00e8c
salaamii <salamiolamide10@gmail.com>
2026-05-09 17:08:43 +0100

    hero on mobile

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -7,22 +7,22 @@ const Hero = ()=> {
             <div className="hero-line"></div>
             <div className="hero-line"></div>
 
-            <div className="text-container flex flex-col gap-6 w-full mt-4 mx-auto max-w-[1000px] pl-[10%]">
-                <div className="hero-eyebrow font-mono text-[1rem] text-green tracking-widest uppercase mb-8 flex items-center gap-3">
+            <div className="text-container flex flex-col gap-6 w-full mt-4 mx-auto max-w-[1000px] px-6 lg:pl-[10%] lg:px-0">
+                <div className="hero-eyebrow font-mono text-[0.8rem] lg:text-[1rem] text-green tracking-widest uppercase mb-8 flex items-center gap-3">
                     <p className="">FRONTEND DEVELOPER - LAGOS. NG</p>
                 </div>
 
-                <h1 className="font-condensed font-extrabold text-[clamp(5rem,16vw,11.5rem)] leading-[0.88] text-white uppercase tracking-[-0.02em] mb-[0.2em]">SAL<span className="name-outline">AMI</span></h1>
+                <h1 className="font-condensed font-extrabold text-[clamp(5rem,14vw,11.5rem)] leading-[0.88] text-white uppercase tracking-[-0.02em] mb-[0.2em] text-center">SAL<span className="name-outline">AMI</span></h1>
 
-                <p className="hero-desc text-text text-[1rem] max-w-[400px] leading-[1.8] mb-12 border-l border-green pl-5">
+                <p className="hero-desc text-text text-[1.2rem] lg:text-[1rem] text-left max-w-[400px] leading-[1.8] mb-12 border-l border-green pl-5">
                     Frontend developer with a background in Statistics and Computer Science.
                      I build clean, functional web applications, and when I'm not writing code,
                      I produce music under <a href="https://freiyardstd.vercel.app" className="about-link text-green underline" target="_blank">DRKCHLD</a>.
                 </p>
 
-                <div className="hero-cta flex gap-6 font-mono font-extrabold tracking-widest">
-                    <button className='border border-text-dim text-bg bg-green py-4 px-8 cursor-pointer'>VIEW WORK</button>
-                    <button className='border border-text-dim text-text-dim py-4 px-8 hover:text-green hover:text-green hover:border-green transition-colors cursor-pointer'>GET IN TOUCH</button>
+                <div className="hero-cta flex flex-col lg:flex-row gap-6 font-mono font-extrabold tracking-widest">
+                    <button className='border border-text-dim text-bg bg-green py-3 px-4 lg:py-4 lg:px-8 cursor-pointer'>VIEW WORK</button>
+                    <button className='border border-text-dim bg-bg text-text-dim py-3 px-4 lg:py-4 lg:px-8 hover:text-green hover:text-green hover:border-green transition-colors cursor-pointer'>GET IN TOUCH</button>
 
                 </div>
                 
